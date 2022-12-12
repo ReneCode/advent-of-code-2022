5a0210483cc701d54a7c743c9fd44991fb614146
Reinhard Langner <langner.r@eplan.de>
2022-12-12 19:43:19 +0100

    day-12 part-1

---
+++ 12.py
@@ -0,0 +1,170 @@
+import util
+
+
+
+def get_data():
+    lines = util.read_lines('./12-example.data')
+    start = None
+    end = None
+    rows = len(lines)
+    cols = len(lines[0])
+    ord_a = ord("a")
+    board = []
+    for r in range(rows):
+        board_row = []
+        for c in range(cols):
+            field = lines[r][c]
+            pos = (r,c)
+            if field == "S":
+                start = pos
+                field = "a"
+            elif field == "E":
+                end = pos
+                field = "z"
+            val = ord(field) - ord_a + 1
+            board_row.append(val)
+        board.append(board_row)
+    return (start, end, board)
+
+
+# elevation (hight) a,b,c ... z
+# start S (has elevation a)
+# best signal E (has elevation z)
+
+# minimal steps to go from S to E
+# next field can only be one higher than the current field
+
+class TableItem:
+    def __init__(self, pos, height):
+        self.pos = pos
+        self.height = height
+        self.steps = None
+        self.prev_pos = None
+
+    def __repr__(self):
+        return f'{self.pos} cost:{self.steps} height:{self.height} prev:{self.prev_pos}'
+
+    def set_steps(self, steps, prev_pos):
+        if self.steps == None or self.steps > steps:
+            self.steps = steps
+            self.prev_pos = prev_pos
+        else:
+            raise Exception(f'do not set item to higher steps on pos:{self.pos}')
+
+# https://www.programiz.com/dsa/dijkstra-algorithm
+"""
+function dijkstra(G, S)
+    for each vertex V in G
+        distance[V] <- infinite
+        previous[V] <- NULL
+        If V != S, add V to Priority Queue Q
+    distance[S] <- 0
+	
+    while Q IS NOT EMPTY
+        U <- Extract MIN from Q
+        for each unvisited neighbour V of U
+            tempDistance <- distance[U] + edge_weight(U, V)
+            if tempDistance < distance[V]
+                distance[V] <- tempDistance
+                previous[V] <- U
+    return distance[], previous[]
+"""
+
+class WayFinder:
+    def __init__(self, start, end, board):
+        self.start_pos = start
+        self.end_pos = end
+        self.board = board
+        self.rows = len(board)
+        self.cols = len(board[0])
+        self.table = {}
+        self.waiting_pos = []
+        self.visited_positions = {}
+
+    def init_table(self):
+        for r in range(self.rows):
+            for c in range(self.cols):
+                pos = (r,c)
+                height = self.board[r][c]
+                item = TableItem(pos, height)
+                if pos == self.start_pos:
+                    item.set_steps(0, None)
+                self.table[pos] = item
+
+    def add_waiting(self, pos):
+        if not pos in self.waiting_pos:
+            self.waiting_pos.append(pos)
+
+    def remove_waiting(self, pos):
+        self.waiting_pos.remove(pos)
+
+    def get_min_waiting(self):
+        # get item with min steps
+        items = [self.get_item(pos) for pos in self.waiting_pos]
+        min_steps = items[0].steps
+        min_item = items[0]
+        for item in items:
+            if item.steps < min_steps:
+                min_steps = item.steps
+                min_item = item
+        return min_item
+
+    def is_visited(self, pos):
+        return self.visited_positions.get(pos) != None
+
+    def add_visited(self, pos):
+        self.visited_positions[pos] = True
+
+
+
+    def get_next_positions(self, pos, height):
+        # next pos has to have next_height <= height+1
+        (r,c) = pos
+        test_positions = []
+        if r > 0:
+            test_positions.append( (r-1,c) )
+        if (r+1) < self.rows:
+            test_positions.append( (r+1,c) )
+        if c > 0:
+            test_positions.append( (r,c-1) )
+        if (c+1) < self.cols:
+            test_positions.append( (r,c+1) )
+
+        next_positions = []
+        for test_pos in test_positions:
+            item = self.get_item(test_pos)
+            if item.height <= (height+1):
+                next_positions.append(test_pos)
+        return next_positions
+
+    def get_item(self, pos):
+        return self.table[pos]
+
+    def calc(self):
+        self.add_waiting(self.start_pos)
+        item = self.get_item(self.start_pos)
+        item.steps = 0
+
+        while len(self.waiting_pos) > 0:
+            current_item = self.get_min_waiting()
+            next_positions = self.get_next_positions(current_item.pos, current_item.height)
+            for next_pos in next_positions:
+                if not self.is_visited(next_pos):
+                    steps = current_item.steps + 1
+                    next_item = self.get_item(next_pos)
+                    if next_item.steps == None or next_item.steps > steps:
+                        next_item.set_steps(steps, current_item.pos)
+                    self.add_waiting(next_pos)
+            self.add_visited(current_item.pos)
+            self.remove_waiting(current_item.pos)
+
+
+
+(start, end, board) = get_data()
+wayFinder = WayFinder(start, end, board)
+wayFinder.init_table()
+wayFinder.calc()
+last_item = wayFinder.get_item(wayFinder.end_pos)
+print(f'shortest way: {last_item.steps}')
+
+# print(start, end, board)
