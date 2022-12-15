a8407ea25848b98f3e6e0dddcb193d7f7b6edd6b
Reinhard Langner <ReinhardLangner@gmx.de>
2022-12-15 18:46:03 +0100

    day-15 part-1

---
+++ 15.py
@@ -0,0 +1,86 @@
+from collections import namedtuple
+
+import util
+
+
+Position = namedtuple('Position', ['x', 'y'])
+
+class Sensor:
+  def __init__(self, sensor_pos, beacon_pos):
+    self.sensor_pos = sensor_pos
+    self.beacon_pos = beacon_pos
+    dx = abs(self.sensor_pos.x - self.beacon_pos.x)
+    dy = abs(self.sensor_pos.y - self.beacon_pos.y)
+    self.dist = dx + dy
+
+  def __repr__(self):
+    return f'sensor:{self.sensor_pos} / beacon:{self.beacon_pos} / dist:{self.dist}'
+
+  def calc_covered_x_values(self, y):
+    delta = abs(y - self.sensor_pos.y)
+    if delta > self.dist:
+      return []
+
+    min_y = self.sensor_pos.y - self.dist
+    max_y = self.sensor_pos.y + self.dist
+    if y < min_y or max_y < y:
+      return []
+
+    dx = abs(self.dist - delta)
+    covered_x_values = [x for x in range(self.sensor_pos.x-dx, self.sensor_pos.x+dx +1)]
+    return covered_x_values
+
+
+
+def get_data():
+  lines = util.read_lines('./15.data')
+  sensors = []
+  bounding_box = util.BoundingBox()
+  for line in lines:
+    tok = line.split(" ")
+    sensor_x = int(tok[2].split("=")[1].split(",")[0])
+    sensor_y = int(tok[3].split("=")[1].split(":")[0])
+    beacon_x = int(tok[8].split("=")[1].split(",")[0])
+    beacon_y = int(tok[9].split("=")[1])
+
+    sensor = Sensor(Position(sensor_x, sensor_y), 
+                    Position(beacon_x, beacon_y))
+    sensors.append(sensor)
+    bounding_box.add_pos(sensor_x, sensor_y)
+    bounding_box.add_pos(beacon_x, beacon_y)
+
+  return sensors
+
+
+sensors = get_data()
+
+total = 0
+y = 2000000
+# y = 10
+# for x in range(bounding_box.min_x, bounding_box.max_x+1):
+#   total += 1
+beacon_xs = [s.beacon_pos.x for s in sensors]
+min_x = min(beacon_xs)
+max_x = max(beacon_xs)
+
+all_x_values = set()
+for sensor in sensors:
+  covered_x_values = sensor.calc_covered_x_values(y)
+  all_x_values.update(set(covered_x_values))
+
+for sensor in sensors:
+  if sensor.sensor_pos.y == y:
+    x = sensor.sensor_pos.x
+    if x in all_x_values:
+      all_x_values.remove(x)
+  if sensor.beacon_pos.y == y:
+    x = sensor.beacon_pos.x
+    if x in all_x_values:
+      all_x_values.remove(x)
+
+all = sorted([x for x in all_x_values])
+# print(all)
+
+count = len(all_x_values)
+
+print(f'part-1 free positions: {count}')
--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 
 # read file into line array
 
+from collections import namedtuple
+
 def read_lines(filename, strip = True):
   with open(filename) as f:
       lines = f.readlines()
@@ -17,3 +19,29 @@ def signum(x):
     return -1
   else:
     return 0
+
+Position = namedtuple('Position', ['x', 'y'])
+
+
+class BoundingBox:
+  def __init__(self):
+    self.min_x = None
+    self.max_x = None
+    self.min_y = None
+    self.max_y = None
+
+  def __repr__(self):
+    return f'{self.min_x},{self.min_y} / {self.max_x},{self.max_y}'
+
+  def add_pos(self, x,y):
+    if self.min_x == None:
+      self.min_x = x
+      self.max_x = x
+      self.min_y = y
+      self.max_y = y
+    else:
+      self.min_x = min(self.min_x, x)
+      self.max_x = max(self.max_x, x)
+      self.min_y = min(self.min_y, y)
+      self.max_y = max(self.max_y, y)
+
